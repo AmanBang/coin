1f83653325e29f873bb3c271a14afc1ae140bba1
AmanBang <66505533+AmanBang@users.noreply.github.com>
2025-01-03 12:21:30 +0530

    a

--- privateKeyHas.cpp
+++ privateKeyHas.cpp
@@ -107,7 +107,7 @@ std::string privateKeyToBitcoinAddress(const std::string& privateKeyHex, bool co
 }
 
 int main() {
-    std::string privateKeyHex = "0000000000000000000000000000000000000000000000000000ade6d7ce3b9b";
+    std::string privateKeyHex = "00000000000000000000000000000000000000000000000000022bd43c2e9354";
     
     std::string compressedAddress = privateKeyToBitcoinAddress(privateKeyHex, true);
     std::string uncompressedAddress = privateKeyToBitcoinAddress(privateKeyHex, false);
